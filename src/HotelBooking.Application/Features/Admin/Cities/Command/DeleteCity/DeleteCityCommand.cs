6abbc432a2399681f1e3ea79df51b72576c04241
Atta <mr.attamazen@gmail.com>
2026-03-04 16:07:33 +0200

    feat(admin/cities): add DeleteCityCommand with soft-delete guard

---
+++ src/HotelBooking.Application/Features/Admin/Cities/Command/DeleteCity/DeleteCityCommand.cs
@@ -0,0 +1,6 @@
+﻿using HotelBooking.Domain.Common.Results;
+using MediatR;
+
+namespace HotelBooking.Application.Features.Admin.Cities.Command.DeleteCity;
+
+public sealed record DeleteCityCommand(Guid Id) : IRequest<Result<Deleted>>;
